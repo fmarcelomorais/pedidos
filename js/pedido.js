53360b68746897102e7b624fc55c7b8169e526c7
Marcelo Morais <marcelomorais.program@gmail.com>
2021-12-26 00:56:52 -0300

    alteracao pedido.js

--- js/pedido.js
+++ js/pedido.js
@@ -1,13 +1,52 @@
 let db = firebase.firestore()
 
-async function cadastrarPedido(){
+const produtos = []
+function incluiProdutoPedido(e){
+    e.preventDefault();    
+    const select = document.getElementById("selectProduto")
+    const indice = select.selectedIndex
+    const nomeProd = select.options[indice].text
+    const valorProd = Number(select.value)
+    let prod = {produto: nomeProd, valor: valorProd}
+    produtos.push(prod)
+    console.log(produtos)
+    soma(e)
+}
+document.getElementById("btnIncluirProd").addEventListener('click', incluiProdutoPedido)
+
+function soma(e){    
+    e.preventDefault()
+   const total = produtos
+                    .map(x => x.valor)
+                    .reduce((total, preco) => total + preco)
+    return total
+}
+//document.getElementById("btnCadPedido").addEventListener("click", soma)
+
+
+function excluirProdutoPedido(e){
+    e.preventDefault();    
+    const select = document.getElementById("selectProduto")
+    produtos.pop(select.value)
+    console.log(produtos)
+}
+document.getElementById("btnExcluirProd").addEventListener('click', excluirProdutoPedido)
+
+
+async function cadastrarPedido(e){
+    e.preventDefault()
     let id = Math.abs(Math.round(Math.random() * (1 - 1000)))
+    const data = document.getElementById("data").value
+    const select = document.getElementById("selectCliente")
+    const cliente = select.value
+    const total = soma(e)
+
     const pedido = {
         id: id,
         data: data,
         cliente: cliente,
-        produto: produto,
-        valor: valor
+        produto: produtos,
+        valor: total
     }
 
     try {
@@ -17,3 +56,27 @@ async function cadastrarPedido(){
         console.log(error)
     }
 }   
+
+document.getElementById("btnCadPedido").addEventListener("click", cadastrarPedido)
+
+async function lista(){
+    //e.preventDefault()
+    const cli = await db.collection('clientes')
+    const dadosCli = await cli.get()
+    dadosCli.forEach(cliente => {
+        let html = `
+        <option value="${cliente.data().nome}">${cliente.data().nome}</option>
+        `
+        document.getElementById('selectCliente').innerHTML += html
+    });
+
+    const prod = await db.collection('produtos')
+    const dadosProd = await prod.get()
+    dadosProd.forEach(produto => {
+        let html = `
+        <option value="${produto.data().preco}">${produto.data().nome} - R$ ${produto.data().preco}</option>
+        `
+        document.getElementById('selectProduto').innerHTML += html
+    });
+}
+
